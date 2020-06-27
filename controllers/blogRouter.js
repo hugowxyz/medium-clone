ce438c769b0fa4ca60fc3cabc7f16731d49e158b
Hugo <phanhuyhugo@gmail.com>
2020-06-27 14:27:13 +0100

    added POST and REMOVE endpoints

--- controllers/blogRouter.js
+++ controllers/blogRouter.js
@@ -14,9 +14,9 @@ blogRouter.get('/', (req, res) => {
     const db = client.db(dbName)
 
     const collection = db.collection('blogs')
-    const data = collection.find({}).toArray((err, data) => {
-      assert.equal(null, err)
-      res.status(200).json(data)
+    const data = collection.find({}).toArray((error, result) => {
+      assert.equal(null, error)
+      res.status(200).json(result)
       client.close()
     })
   })
@@ -30,18 +30,46 @@ blogRouter.get('/:id', (req, res, next) => {
 
     const queryId = new mongo.ObjectID(req.params.id)
 
-    db.collection('blogs').findOne({_id: queryId}, (err, data) => {
-      if (err) next(err)
-      logger.info(data)
-      res.status(200).json(data)
+    db.collection('blogs').findOne({_id: queryId}, (error, result) => {
+      if (error) next(error)
+      logger.info(result)
+      res.status(200).json(result)
       client.close()
-    })
-    
+    })  
   })
 })
 
 blogRouter.post('/', (req, res) => {
-  
+  const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect((err, client) => {
+    assert.equal(null, err)
+    const db = client.db(dbName)
+
+    const body = req.body
+
+    db.collection('blogs').insertOne(body, (error, result) => {
+      if (error) next(error)
+      res.status(200).json(result)
+      client.close()
+    })    
+  })
+})
+
+blogRouter.delete('/:id', (req, res) => {
+  const client = new MongoClient(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect((err, client) => {
+    assert.equal(null, err)
+    const db = client.db(dbName)
+
+    const queryId = new mongo.ObjectID(req.params.id)
+
+    db.collection('blogs').removeOne({_id: queryId}, (error, result) => {
+      if (error) next(error)
+      res.status(200).json(result)
+      client.close()
+    })
+    
+  })
 })
 
 module.exports = blogRouter
