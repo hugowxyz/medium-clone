89aa39d839091b5473223815aa0e6e7b9fcfb2cd
Hugo <phanhuyhugo@gmail.com>
2020-07-07 11:50:17 +0100

    focus on UI

--- README.md
+++
@@ -1,3 +0,0 @@
-# medium-clone
-
-I am remaking Medium - https://medium.com/ - using the MERN stack for educational purposes.
--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const logger = require('./utils/logger')
-
-const port = process.env.port || 3001
-app.listen(port)
-
-logger.info(`Server running on ${port}`)
